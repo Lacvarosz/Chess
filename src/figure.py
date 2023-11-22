955ec589aaadb84c81e482f4ea629a53539d68e0
Lacvarosz <lagrantaur@gmail.com>
2023-11-22 12:52:55 +0100

    Only Knight is missing

--- src/figure.py
+++ src/figure.py
@@ -20,38 +20,41 @@ class Pawn(Figure):
     def __init__(self, x: int, y: int, color: Site) -> None:
         super().__init__(x, y, color)
         self.base_step_in_previous_round = False
+        
+    def __getitem__(self, x: int, y: int) -> bool:
+        return self.simple_step(x, y)
     
     def simple_step(self, x :int, y:int) -> bool:
         if self.color == Site.WHITE:
             l = x == self.x and y + 1 == self.y
             if l:
-                self[x, y]
+                super()[x, y]
         else:
             l = y - 1 == self.y and x == self.x
             if l:
-                self[x, y]
+                super()[x, y]
         return(l)
     
     def takes(self, x :int, y :int) ->bool:
         if self.color == Site.WHITE:
             l = abs(x - self.x) == 1 and y + 1 == self.y
             if l:
-                self[x, y]
+                super()[x, y]
         else:
             l = abs(x - self.x) == 1 and y - 1 == self.y
             if l:
-                self[x, y]
+                super()[x, y]
         return(l)
     
     def base_step(self, x :int, y :int) ->bool:
         if self.color == Site.WHITE:
             l = x == self.x and y + 2 == self.y and y == 6
             if l:
-                self[x, y]
+                super()[x, y]
         else:
             l = y - 2 == self.y and x == self.x and y == 1
             if l:
-                self[x, y]
+                super()[x, y]
         if l:
             self.base_step_in_previous_round = True
         return(l)
@@ -67,10 +70,13 @@ class King(Figure):
         super().__init__(x, y, color)
         self.didnt_move = True
     
+    def __getitem__(self, x: int, y: int) -> bool:
+        return(self.step(x, y))
+    
     def step(self, x :int, y :int) -> bool:
         l = max(abs(self.x -x), (self.y - y)) == 1
         if l:
-            self[x, y]
+            super()[x, y]
             self.didnt_move = False
         return(l)
     
@@ -78,17 +84,45 @@ class King(Figure):
         l = self.didnt_move and abs(self.x - x) == 2 and self.y == y
         if l:
             self.didnt_move = False
-            self[x, y]
+            super()[x, y]
         return(l)
 
 class Queen(Figure):
     def __init__(self, x: int, y: int, color: Site) -> None:
         super().__init__(x, y, color)
     
+    def __getitem__(self, x: int, y: int) -> bool:
+        return self.step(x, y)
+    
     def step(self, x :int, y :int) -> bool:
         l = (x == self.x or y == self.y) or (abs(self.x - x) == abs(self.y - y))
         if l:
-            self[x, y]
+            super()[x, y]
+        return(l)
+
+class Rook(Figure):
+    def __init__(self, x: int, y: int, color: Site) -> None:
+        super().__init__(x, y, color)
+    
+    def __getitem__(self, x: int, y: int) -> bool:
+        return self.step(x, y)
+    
+    def step(self, x :int, y :int) -> bool:
+        l = x == self.x or y == self.y
+        if l:
+            super()[x, y]
         return(l)
     
+class Bishop(Figure):
+    def __init__(self, x: int, y: int, color: Site) -> None:
+        super().__init__(x, y, color)
+    
+    def __getitem__(self, x: int, y: int) -> bool:
+        return self.step(x, y)
+    
+    def step(self, x :int, y :int) -> bool:
+        l = (abs(self.x - x) == abs(self.y - y))
+        if l:
+            super()[x, y]
+        return(l)
     
